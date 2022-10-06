36d1a0400f0962580535d22fba5b1522d60c84e1
16HSleslie <stanhl1@student.op.ac.nz>
2022-10-06 19:19:44 +1300

    commit 4

--- public/script.js
+++ public/script.js
@@ -21,56 +21,54 @@ const displayFact = async () => {
     console.log(fact);
     document.getElementById('jokeContainer').innerHTML = fact[0].setup + ' ' + fact[0].punchline;
 }
+
 displayFact();
 
-class Song {
-  constructor(name, songLink, albumCover) {
-    this._name = name;
-    this._songLink = songLink;
-    this._albumCover = albumCover;
-  }
+const urlTwo = 'http://127.0.0.1:3000';
+const routeTwo = '/getSongs';
 
-  get songName() {
-    return this._name;
+const getSongs = async () => {
+  const endpoint = urlTwo + routeTwo;
+  let jsonResponse;
+  try {
+    const response = await fetch(endpoint, {cache: 'no-cache'});
+    if (response.ok) {
+      jsonResponse = await response.json();
+      console.log(jsonResponse);
+    }
+  } catch(error) {
+    console.log(error);
   }
+  return jsonResponse;
+}
 
-  get songLink() {
-    return this._songLink;
-  }
+let songData;
 
-  get albumCover() {
-    return this._albumCover;
-  }
+const saveSongData = async () => {
+  songData = await getSongs();
+  console.log(songData);
 };
 
-const alcoholic = new Song('alcoholic', '/songs/homebrewski-alcoholic.mp3', '/pictures/home-brew.jpg');
-const blowingSmoke = new Song('blowing smoke', '/songs/homeBrewski-blowingSmoke.mp3', '/pictures/b-sides.jpg');
-const listenToUs = new Song('listen to us', '/songs/homebrewski-listenToUs.mp3', '/pictures/home-brew.jpg');
-const stateOfMind = new Song('state of mind', '/songs/homebrewski-stateOfMind.mp3', '/pictures/home-brew.jpg');
-const sunday = new Song('sunday', '/songs/homebrewski-sunday.mp3', '/pictures/last-week.jpg');
-const songs = [alcoholic, blowingSmoke, listenToUs, stateOfMind, sunday];
+saveSongData();
 
 let score = 0;
-var songChoice = Math.ceil(Math.random() * 5);
+let songChoice = songData[Math.floor(Math.random() * 5)];
 const audioSound = document.getElementById("audioSound");
 const answerPicture = document.getElementById("answerPicture");
 const albumCover = document.getElementById("albumCover");
-audioSound.src = songs[songChoice].songLink;
-audioSound.load();
-albumCover.src = songs[songChoice].albumCover;
+changeSong();
 
 function changeSong() {
-  songChoice = Math.ceil(Math.random() * 5);
-  audioSound.src = songs[songChoice].songLink;
+  audioSound.src = songChoice.song_path;
   audioSound.load();
-  albumCover.src = songs[songChoice].albumCover;
+  albumCover.src = songChoice.photo_path;
 };
 
 function guessingGame() {
   let input = document.getElementById("guessInputBox").value;
   input = input.toLowerCase();
   if (input) {
-    if (input === songs[songChoice].songName) {
+    if (input.toLowerCase() === songChoice.song_name.toLowerCase()) {
         alert('correct')
         score++;
         alert("that took " + score + " attempts");
